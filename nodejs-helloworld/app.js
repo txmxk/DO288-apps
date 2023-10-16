96dabb09cf54de30808472cbe24731d0cbb5bc67
txmxk <redhatdrone@gmail.com>
2023-10-15 19:23:59 -0500

    per aspera

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Per aspera ad astra!\n');
 });
 
 app.listen(8080, function () {
